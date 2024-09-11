eb608c5478b0e1504cabe50ff902ec68eb3e34d8
Sasha Bédard <85566308+sashabedard@users.noreply.github.com>
2024-09-11 12:17:09 -0400

    started ex1

---
+++ Exercice 1/Exercice1/Exercice1.ino
@@ -0,0 +1,35 @@
+// DISPLAY
+const int pinA = 0;
+const int pinB = 1;
+const int pinC = 2;
+const int pinD = 3;
+const int pinE = 4;
+const int pinF = 5;
+const int pinG = 6;
+const int pinH = 7;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pinA, OUTPUT);
+  pinMode(pinB, OUTPUT);
+  pinMode(pinC, OUTPUT);
+  pinMode(pinD, OUTPUT);
+  pinMode(pinE, OUTPUT);
+  pinMode(pinF, OUTPUT);
+  pinMode(pinG, OUTPUT);
+  pinMode(pinH, OUTPUT);
+  }
+
+void loop() {
+
+  digitalWrite(pinA, 0);
+  digitalWrite(pinB, 0);
+  digitalWrite(pinC, 0);
+  digitalWrite(pinD, 0);
+  digitalWrite(pinE, 0);
+  digitalWrite(pinF, 0);
+  digitalWrite(pinG, 0);
+  digitalWrite(pinH, 0);
+
+}
